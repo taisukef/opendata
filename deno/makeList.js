25a14a0bf8d8b4086008ff185c51bd7265b0188e
Taisuke Fukuno <fukuno@jig.jp>
2021-08-26 06:34:15 +0900

    to be simple

--- deno/makeList.js
+++ deno/makeList.js
@@ -10,18 +10,15 @@ for (const d of data) {
     continue;
   }
   const list = await CSV.fetch("../" + d);
-  console.log(list[0]);
   const head = { csv: d };
   if (list[0][0] == "#LINK") {
     for (const l of list) {
       if (l[0] == null || l[0] == "" || l[0] == "#property") {
         break;
       }
-      console.log(l[0])
       head[l[0].substring(1)] = l[1];
     }
   }
-  //console.log(head);
   res.push(head);
 }
-await Deno.writeTextFile("opendata.csv", CSV.encode(CSV.fromJSON(res)));
+await Deno.writeTextFile("opendata.csv", CSV.stringify(res));
